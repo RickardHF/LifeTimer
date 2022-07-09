716e8ca7c609b77e4dd6824075132f24434234ab
Rickard Hole Falck <rickardhole.falck@gmail.com>
2022-07-09 11:48:03 +0100

    added some stuff

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,85 @@
 <template>
+  <section class="std-section">
+    <h1>How long will I live?</h1>
+    <p>
+      Many of us spend time worrying how short life is, while others rather not think about it at all or think they'll live forever. Death is a difficult question for us humans, yet 
+      it's something awaiting us all. While it is not good to ponder on ones own death at all times or to give one self to despair, it can also be bad 
+      not to consider how valueable time is.
+    </p>
+    <figure>
+      <blockquote class="citation">
+        <h2>
+          "Think of your self as dead, you have lived your life. Now take what's left and live it properly"
+        </h2>
+      </blockquote>
+      <cite>Marcus Aurelius</cite>
+    </figure>
+    <p>
+      Our aim is here to give a usefull perspective on life. Seing how much or little is left of ones life in clear numbers can for many be a real eye opener.
+    </p>
+    <br/>
+  </section>
   <section class="highlighted-section" key="checkform">
-    <h1 class="gradient-header">How long do I have to live?</h1>
     <InformationForm></InformationForm>
   </section>
+  <section class="std-section">
+    <figure>
+      <blockquote class="citation">
+        <h2>
+          "It is not death that a man should fear, but he should fear never beginning to live."
+        </h2>
+      </blockquote>
+      <cite>Marcus Aurelius</cite>
+    </figure>
+  </section>
+  <section class="lighter-section std-section">
+    <h1>FAQ</h1>
+    <div class="faq-container">
+      <!--
+
+        <details>
+          <summary>How accurate is this prediction?</summary>
+        <p>
+          This prediction is only based on the life expectancy of your nationality for your gender. There are many other factors determinating how long you will
+          live, such as health, genes, health habits, smoking, drinking etc.
+        </p>
+      </details>
+      <details>
+        <summary>I have only a few years left, what now?</summary>
+        <p>
+          This estimation is by no means a definitive answer to how long left you have to live, and is derrived only out of the life expectancy. Don't get too troubled with 
+          the results. If you are worried about this, contact your doctor to get a better evaluation.
+        </p>
+      </details>
+      <details>
+        <summary>I have surpassed the life expectancy, what now?</summary>
+        <p>
+          Congratulations, you have lived longer than what is expected! Don't worry too much about the results, you might still have many more years left.
+          If you are worried about this, contact your doctor to get a better evaluation.
+        </p>
+      </details>
+      -->
+      <p class="faq-header">How accurate is this prediction?</p>
+      <p>
+        This prediction is only based on the life expectancy of your nationality for your gender. There are many other factors determinating how long you will
+        live, such as health, genes, health habits, smoking, drinking etc.
+      </p>
+      <p class="faq-header">I have only a few years left, what now?</p>
+      <p>
+        This estimation is by no means a definitive answer to how long left you have to live, and is derrived only out of the life expectancy. Don't get too troubled with 
+        the results. If you are worried about this, contact your doctor to get a better evaluation.
+      </p>
+      <p class="faq-header">I have surpassed the life expectancy, what now?</p>
+      <p>
+        Congratulations, you have lived longer than what is expected! Don't worry too much about the results, you might still have many more years left.
+        If you are worried about this, contact your doctor to get a better evaluation.
+      </p>
+    </div>
+    <br/>
+    <br/>
+    <br/>
+  </section>
+  <footer>mh.</footer>
 </template>
 
 <script>
@@ -17,6 +94,41 @@ export default {
 </script>
 
 <style>
+
+details {
+  width: 100%;
+  max-width: 650px;
+  margin-top: 2rem;
+}
+
+details p {
+  width: 100%;
+}
+
+details>summary { 
+  list-style: none;
+}
+summary::after {
+  content: ' ►';
+}
+details[open] summary:after {
+  content: " ▼";
+}
+
+summary {
+  font-size: larger;
+  font-weight: bold;
+}
+
+.faq-header {
+  font-size: larger;
+  font-weight: bold;
+}
+
+.faq-header p {
+  width: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -30,8 +142,28 @@ export default {
 
 }
 
+p {
+  width: 80%;
+  max-width: 650px;
+}
+
+.citation {
+  max-width: 400px;
+}
+
+figure {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h2, p, h3, h1, h4, input, button, select {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
 .gradient-header {
-    background: -webkit-linear-gradient(300deg, #ddd9d5 20%, #f8cfcf 70%);
+    background: -webkit-linear-gradient(300deg, var(--dark-color) 20%, #9c67ff 70%);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     -webkit-box-decoration-break: clone;
@@ -44,7 +176,24 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: #FFFFFF25;
+  background-color: #ffffff;
+}
+
+.lighter-section {
+  background-color: #FFFFFF1D;
+}
+
+.std-section {
+  color: var(--text-background);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: left;
+  font-size: larger;
+}
+
+footer {
+  background-color: var(--text-background);
 }
 
 </style>
--- src/components/InformationForm.vue
+++ src/components/InformationForm.vue
@@ -5,15 +5,64 @@
         <h3>Congratulations!</h3>
         <h4>You have already lived longer than the average in your country</h4>
         <h3>You have {{daysInExcess}}</h3>
+        <div class="countdown-calender">
+          <div class="calender-item">
+            {{daysInExcess.years}}
+            <p>Years</p>
+          </div>
+          <div class="calender-item">
+            {{daysInExcess.days}}
+            <p>Days</p>
+          </div>
+          <div class="calender-item">
+            {{daysInExcess.hours}}
+            <p>Hours</p>
+          </div>
+          <div class="calender-item">
+            {{daysInExcess.minutes}}
+            <p>Minutes</p>
+          </div>
+          <div class="calender-item">
+            {{daysInExcess.seconds}}
+            <p>Seconds</p>
+          </div>
+        </div>
       </div>
       <div class="time-to-go" v-else>
+        <h1 class="gradient-header">
+          Watch as your time is running out!
+        </h1>
         <p>You will die at {{expectedDeath}}</p>
-        <h3>You have {{daysToGo}}</h3>
+        <div class="countdown-calender">
+          <h3>Remaining time:</h3>
+          <div class="calender-item">
+            {{daysToGo.years}}
+            <p>Years</p>
+          </div>
+          <div class="calender-item">
+            {{daysToGo.days}}
+            <p>Days</p>
+          </div>
+          <div class="calender-item">
+            {{daysToGo.hours}}
+            <p>Hours</p>
+          </div>
+          <div class="calender-item">
+            {{daysToGo.minutes}}
+            <p>Minutes</p>
+          </div>
+          <div class="calender-item">
+            {{daysToGo.seconds}}
+            <p>Seconds</p>
+          </div>
+        </div>
       </div>
-      <button :onclick="goback">Go Back</button>
+      <button :onclick="goback"> ⤺ Go Back</button>
     </div>
     <div class="form-filling" v-else>
-      <h2>Fill in you info</h2>
+      <h2 class="gradient-header">How long do I have to live?</h2>
+      <p>Use our lifeexpectancy calculator to estimate how long you have left to live!</p>
+      <br/>
       <div class="information-row">
         <label for="nationality">Nationality</label>
         <select id="nationality" v-model="selectedCountry">
@@ -32,11 +81,11 @@
         <div class="information-cont-col">
           <div class="information-cont-row">
             <input type="radio" id="man" :value="true" v-model="man"/>
-            <label for="man">Male</label>
+            <label for="man">♂ Male</label>
           </div>
           <div class="information-cont-row">
             <input type="radio" id="woman" :value="false" v-model="man" title="Female"/>
-            <label for="woman">Female</label>
+            <label for="woman">♀ Female</label>
           </div>
         </div>
       </div>
@@ -143,9 +192,46 @@ export default {
 
 <style>
 
+.countdown-calender {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  justify-content: center;
+  background-color: var(--dark-color-lig);
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  filter: drop-shadow(.3rem .3rem .6rem #000000a0)
+}
+
+.countdown-calender h3 {
+  width: 100%;
+  color: var(--text-background);
+}
+
+.calender-item {
+  margin: .5rem;
+  padding: .5rem;
+  min-width: 4rem;
+  font-size: 1.2rem;
+  color: var(--dark-color);
+  border-radius: .5rem;
+  background-color: var(--text-background);
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+  filter: drop-shadow(.1rem .1rem .3rem #000000a0)
+}
+
+.calender-item p {
+  font-size: .8rem;
+  color: var(--dark-color-lig);
+}
+
 button {
   transition-duration: 0.4s;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   background-color: var(--dark-color);
   border-radius: 5px;
   padding: 5px;
@@ -162,19 +248,13 @@ button:disabled {
   color: var(--text-color-dis);
 }
 
-h2 {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}
-
 select {
   text-align: center;
   padding: .1rem;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 
 input {
   padding: .1rem;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 
 option {
@@ -190,9 +270,9 @@ option {
   display: flex;
   justify-content: center;
   flex-direction: column;
-  /*border: .2rem solid rgb(89, 84, 212);*/
+  /*border: .2rem solid rgb(89, 84, 212);
   border-radius: .4rem;
-  filter: drop-shadow(.2rem .2rem 1rem #000 )
+  filter: drop-shadow(.2rem .2rem 1rem #000 )*/
 
 }
 
